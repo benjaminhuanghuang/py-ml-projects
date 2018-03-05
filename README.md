115224fbe2a517561f279ec10d003da6dba48f0e
Benjamin Huang <bhuang@rms.com>
2018-03-05 10:29:11 -0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## Reference
     - Python Machine Learning Projects -Udemy
+    - https://my.oschina.net/leejun2005/blog/407043
 
 ## Setup
 ```
@@ -11,4 +12,27 @@ $ . venv3/bin/activate
 
 ...
 (venv3)$ deactivate
+
+brew install phantomjs
 ```
+
+
+## About Selenium, chromedriver and PhantomJS
+Install selenium
+```
+pip install selenium
+```
+Using PhantomJS
+```
+driver = webdriver.PhantomJS()
+```    
+causes warning
+```
+Selenium support for PhantomJS has been deprecated, please use headless versions of Chrome or Firefox instead
+```
+So PhantomJS should be replaced with chrome driver. To fix error
+```
+selenium.common.exceptions.WebDriverException: Message: ‘chromedriver’ executable needs to be in PATH.
+```
+We should download chromedriver at https://sites.google.com/a/chromium.org/chromedriver/downloads, when put it into 
+virtual environment/bin
--- fare_alerter.py
+++ fare_alerter.py
@@ -1,38 +1,52 @@
+'''
+    Find cheap airfares
+    1. retrieving fare data with web scraping techniques
+    2. parse the DOM to extract pricing data
+    3. sending real-time alters using IFTTT
+'''
+import pandas as pd
+import numpy as np
+
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.by import by
+# from selenium.webdriver.common.by import by
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
 
-from sklearn.cluster imort DBSCAN
+from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
-
-from schedule
-from time
 
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt 
 
 
+
+
 def check_flights():
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1200x600')
+
+    driver = webdriver.Chrome(chrome_options=options)
+    # dcap = dict(DesiredCapabilities.PHANTOMJS)
+    # dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36")
+    
     url ="https://www.google.co.in/flights/explore/#explore;f=JFK,EWR,LGA;t=r-Europe-0x46ed8886cfadda85%253A0x72ef99e6b3fcf079;li=8;lx=12;d=2016-11-29"
-    driver = webdriver.PhantomJS()
-    dcap = dict(DesiredCapabilities.PHANTOMJS)
-    dcap["phantomjs.page.settings.userAgent"] = ("Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36")
-    driver = webdriver.PhantomJS(desired_capabilities=dcap,service_args=['--ignore-ssl-errors=true'])
-    driver.implicitly_wait(20)
     driver.get(url)
+    # driver.save_screenshot('flight_explorer.png')
+    # wait = WebDriverWait(driver, 20)
+    # wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span.LH3SCIC-v-c')))
 
-    wait = WebDriverWait(driver, 20)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'span.LH3SCIC-v-c')))
-
-    s = BeautifulSoup(driver.page_source, "lxml")
-    best_price_tags = s.findAll('div', 'LH3SCIC-w-e')
+    # s = BeautifulSoup(driver.page_source, "lxml")
+    # best_price_tags = s.findAll('div', 'LH3SCIC-w-e')
     
     # check if scrape worked  - alert ifit fails and shutdown
-    if len(best_price_tags) < 4:
-        print('Failed to load page data')
+    # if len(best_price_tags) < 4:
+    #     print('Failed to load page data')
+        
+if __name__ == '__main__':
+    check_flights()
